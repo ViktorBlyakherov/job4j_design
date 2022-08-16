2d9119c80b998beba88a850e4cdf090c4175143a
Viktor Blyakherov <xxxxxvic@yandex.ru>
2022-08-16 16:56:15 +0300

    2. Анализ доступности сервера. [#859] ver2

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -6,41 +6,31 @@ import java.util.List;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-        List<String> rsl = new ArrayList<>();
-        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source)); PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
             String beginPeriod = "";
             String endPeriod = "";
             String period = "";
             for (String line = in.readLine(); line != null; line = in.readLine()) {
                 int status = Integer.parseInt(line.substring(0, 3));
-                period = line.substring(4, line.length());
+                period = line.substring(4);
                 if (status == 400 || status == 500) {
                     if (beginPeriod.isEmpty()) {
                         beginPeriod = period;
                     }
                 } else if (endPeriod.isEmpty() && !beginPeriod.isEmpty()) {
                     endPeriod = period;
-                    rsl.add(beginPeriod + ";" + endPeriod + ";");
+                    out.println(beginPeriod + ";" + endPeriod + ";");
                     beginPeriod = "";
                     endPeriod = "";
                 }
             }
 
             if (!beginPeriod.isEmpty() && endPeriod.isEmpty()) {
-                rsl.add(beginPeriod + ";" + period + ";");
+                out.println(beginPeriod + ";" + period + ";");
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-            for (String str : rsl) {
-                out.println(str);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
     }
 
     public static void main(String[] args) {
