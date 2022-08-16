be6b447a934e45b479b4c40cae9ad553919a35c4
Viktor Blyakherov <xxxxxvic@yandex.ru>
2022-08-16 22:23:16 +0300

    3.0. Тестирование IO [#173905]

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,7 +1,30 @@
 package ru.job4j.io;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import java.io.*;
+import java.nio.file.Path;
+import java.util.List;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class AnalizyTest {
+    @Test
+    void drop(@TempDir Path tempDir) throws IOException {
+        File source = tempDir.resolve("server.log").toFile();
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("500 10:59:01");
+            out.println("400 11:01:02");
+            out.println("200 11:02:02");
+        }
+        File target  = tempDir.resolve("unavailable.csv").toFile();
+        new Analizy().unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat("10:57:01;11:02:02;").isEqualTo(rsl.toString());
+    }
 
 }
