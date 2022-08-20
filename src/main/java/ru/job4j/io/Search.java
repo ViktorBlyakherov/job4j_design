c2c0e8cfc5c5583398e9c4eab54a3972965fecaa
Viktor Blyakherov <xxxxxvic@yandex.ru>
2022-08-20 15:08:02 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -18,6 +19,18 @@ public class Search {
         if (args.length < 2) {
             throw new IllegalArgumentException("You have to specify root folder and file extension to search. "
                     + ". Usage java -jar search.jar ROOT_FOLDER .FILE_EXTENSION");
+        }
+
+        File file = new File(args[0]);
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+
+        if (args[1].charAt(0) != '.') {
+            throw new IllegalArgumentException(String.format("Not a file extension %s", args[1]));
         }
     }
 
