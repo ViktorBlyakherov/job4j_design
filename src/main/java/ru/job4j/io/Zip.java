eaf42452b20412def5e4188ec5dc0e64134152ab
Viktor Blyakherov <xxxxxvic@yandex.ru>
2022-08-22 22:43:17 +0300

    5.2. Архивировать проект [#861 #334674]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -10,11 +10,11 @@ import static ru.job4j.io.Search.search;
 
 public class Zip {
 
-    public void packFiles(List<File> sources, File target) {
+    public void packFiles(List<Path> sources, File target) {
             try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-                for (File source : sources) {
-                    zip.putNextEntry(new ZipEntry(source.getPath()));
-                    try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                for (Path source : sources) {
+                    zip.putNextEntry(new ZipEntry(source.toString()));
+                    try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source.toFile()))) {
                         zip.write(out.readAllBytes());
                     }
                 }
@@ -23,43 +23,31 @@ public class Zip {
             }
     }
 
-    public void packSingleFile(File source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
-                zip.write(out.readAllBytes());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     private static void checkArgs(ArgsName names) {
-        if (names.get("d") == null || names.get("e") == null || names.get("o") == null) {
-            throw new IllegalArgumentException("Invalid parameters. "
-                    + "Use java -jar pack.jar -d=PATH -e=.EXT -o=ARCHIVE_NAME");
-        }
-
         File file = new File(names.get("d"));
         if (!file.exists()) {
-            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+            throw new IllegalArgumentException(String.format("Not exist %s", names.get("d")));
         }
         if (!file.isDirectory()) {
-            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+            throw new IllegalArgumentException(String.format("Not directory %s", names.get("d")));
         }
 
-        if (names.get("o").charAt(0) != '.') {
+        if (names.get("e").charAt(0) != '.') {
             throw new IllegalArgumentException(String.format("Not a file extension %s", names.get("o")));
         }
 
     }
 
     public static void main(String[] args) throws IOException {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Invalid parameters. "
+                    + "Use java -jar pack.jar -d=PATH -e=.EXT -o=ARCHIVE_NAME");
+        }
         ArgsName names = ArgsName.of(args);
         checkArgs(names);
         Zip zip = new Zip();
         File target = new File(names.get("o"));
         List<Path> paths = Search.search(Path.of(names.get("d")), p -> !p.toFile().getName().endsWith(names.get("e")));
-        zip.packFiles(paths.stream().map(Path::toFile).toList(), target);
+        zip.packFiles(paths, target);
     }
 }
