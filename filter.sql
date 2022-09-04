8255a004f5803dc59864ba0dec59f8729dcd40e3
Viktor Blyakherov <xxxxxvic@yandex.ru>
2022-09-04 16:05:42 +0300

    3. Фильтры [#1732]

---
+++ filter.sql
@@ -0,0 +1,43 @@
+create table product(
+    id serial primary key,
+    name varchar(255),
+    type_id INT REFERENCES type(id),
+	expired_date date,
+	price float
+);
+
+create table type(
+    id serial primary key,
+    name varchar(255)
+);
+
+insert into type (name) values ('Молоко'), ('Сыр'), ('Мясо');
+
+select * from type;
+
+insert into product (name, type_id, expired_date, price) values
+		('Сметана', 1, '10-09-2022', 120),
+		('Йогурт', 1, '03-09-2022', 80),
+		('Мороженое', 1, '08-09-2022', 50),
+		('Сыр плавленный', 2, '10-09-2022', 30),
+		('Моцарелла', 2, '15-09-2022', 250),
+		('Говядина', 3, '06-09-2022', 500),
+		('Свинина', 3, '01-09-2022', 400);
+
+select * from product;
+
+select * from product p join type t ON t.id = p.type_id where t.name = 'Сыр';
+
+select * from product p where p.name like '%Мороженое%';
+
+select * from product p where p.expired_date < '04-09-2022';
+
+select p.* from product p Order by p.price desc limit 1;
+
+select t.name, count (p.id) from type t join product p on p.type_id = t.id group by t.name;
+
+select p.* from product p join type t on p.type_id = t.id where t.name = 'Сыр' or t.name = 'Молоко';
+
+select t.name from type t JOIN product p on t.id = p.type_id group by t.name having count(p.id) < 10;
+
+select p.name, p.price, p.expired_date, t.name Тип from product p join type t on p.type_id = t.id;
