6e6c2a1b5747bb38dc1b220a226cb548e904fdcb
Viktor Blyakherov <xxxxxvic@yandex.ru>
2022-09-04 19:52:32 +0300

    1. Inner join [#373617 #338534]

--- innerjoin.sql
+++ innerjoin.sql
@@ -1,3 +1,25 @@
+CREATE TABLE IF NOT EXISTS public.cars
+(
+    id integer NOT NULL DEFAULT nextval('cars_id_seq'::regclass),
+    name character varying(255) COLLATE pg_catalog."default",
+    jeep boolean,
+    engine_id integer,
+    CONSTRAINT cars_pkey PRIMARY KEY (id),
+    CONSTRAINT cars_engine_id_fkey FOREIGN KEY (engine_id)
+        REFERENCES public.engines (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+CREATE TABLE IF NOT EXISTS public.engines
+(
+    id integer NOT NULL DEFAULT nextval('engines_id_seq'::regclass),
+    name character varying(255) COLLATE pg_catalog."default",
+    volume double precision,
+    CONSTRAINT engines_pkey PRIMARY KEY (id)
+)
+
+
 select * from cars;
 
 select * from engines;
@@ -11,5 +33,16 @@ INSERT into cars ("name", jeep, engine_id) VALUES ('Skoda', false, 2);
 INSERT into cars ("name", jeep, engine_id) VALUES ('Mercedes', false, 3);
 INSERT into cars ("name", jeep) VALUES ('Nissan', true);
 
-select c.name Название, e.name Описание, e.volume Объем from cars c join engines e on c.engine_id = e.id;
+select c.name Название, e.name Описание
+from cars c join engines e
+on c.engine_id = e.id;
+
+select c.name Название, e.volume Объем
+from cars c join engines e
+on c.engine_id = e.id;
+
+select c.name Название, c.jeep Джип,
+e.name Двигатель,
+from cars c join engines e
+on c.engine_id = e.id;
 
